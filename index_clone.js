51ee35c737856aa5bef2643c155f5d20a5ab5cf8
josiahyoong <josiahyoong@gmail.com>
2023-05-04 19:33:06 +0800

    zoomUI

--- index_clone.js
+++ index_clone.js
@@ -17,7 +17,7 @@ async function getTaxiData() {
       "https://api.data.gov.sg/v1/transport/taxi-availability"
     );
     // const response = await fetch("https://api.data.gov.sg/v1/transport/taxi-availability");
-    
+
     if (!response.data) {
       throw new Error("Failed to fetch data");
     }
@@ -56,13 +56,6 @@ function onGetLocation(position) {
     .openPopup();
 }
 
-//     // Show map with markers for taxi location
-//     L.marker([latitude, longitude]).addTo(map)
-//       .bindPopup('My Location')
-//       .openPopup();
-
-//   }
-
 // Handle "Get my location" button click
 function handleGetLocation() {
   if (navigator.geolocation) {
@@ -78,7 +71,7 @@ function handleGetLocation() {
 
 async function getRefresh() {
   const data = await getTaxiData();
-  
+
   const taxiAvailabilities = data.features[0].geometry.coordinates;
   //console.log("🚀 ~ file: index.js:26 ~ window.document.addEventListener ~ taxiAvailabilities:", taxiAvailabilities)
   for (let c of taxiAvailabilities) {
@@ -96,36 +89,78 @@ window.document.addEventListener("DOMContentLoaded", async function () {
   // do a setInterval, setTimeout
   //store in callstack or some computer memory
   let btnGo = document.getElementById("submit-button");
-  btnGo.addEventListener("click", function(){
+  btnGo.addEventListener("click", function () {
     //retrieve the postal code from the textbox
     // get the value of the texbox
-      const input = document.getElementById("text-input").value;
-      console.log("Input value:", input);
-      temp(input);
-  })
-  
+    const input = document.getElementById("text-input").value;
+    console.log("Input value:", input);
+    getPostalCode(input);
+  });
 
-  let buttonClick = document.getElementById("trigger");
+  let buttonClick = document.getElementById("btn1");
   buttonClick.addEventListener("click", handleGetLocation);
+
+  //for the form input and control
+  let submitbtn = document.querySelector("#submit-form");
+  submitbtn.addEventListener("click", function () {
+    const email = document.querySelector("#exampleFormControlInput1").value;
+    if (!validateEmail(email)) {
+      alert("Email is not valid");
+    } 
+  });
 });
 
-async function temp(input) {
+//validation
+function validateEmail(email) {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(email);
+}
+
+function valiateComment(comment){
+  if(comment.length < 0){
+    return false;
+  }else{
+    //meaning that there are comments in the textbox
+    return true;
+  }
+}
+
+async function getPostalCode(input) {
   const postalCode = input; // Replace with the actual postal code you want to convert
   const url = `https://nominatim.openstreetmap.org/search.php?q=${postalCode},Singapore&format=json`;
-
   // const response = await axios.get(url);
   // console.log("🚀 ~ file: index_clone.js:110 ~ temp ~ response:", response.data);
-  
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const lat = data[2].lat;
       const lon = data[2].lon;
       console.log(`Latitude: ${lat}, Longitude: ${lon}`);
-      L.marker([lat, lon]).addTo(map).bindPopup("My search location").openPopup();
+
+     L.marker([lat, lon])
+        .addTo(map)
+        .bindPopup("Postal Code")
+        .openPopup();
     })
     .catch((error) => console.error(error));
+
 }
+
+// Create a new zoom control with a custom position
+var zoomControl = L.control.zoom({ position: 'bottomright' });
+// Add the new zoom control to the map
+zoomControl.addTo(map);
+
+// Create a new zoom control with a custom position
+var zoomControl = L.control.zoom({ position: 'bottomleft' });
+// Add the new zoom control to the map
+zoomControl.addTo(map);
+
+// Get a reference to the default zoom control
+var defaultZoomControl = map.zoomControl;
+// Remove the default zoom control from the map
+map.removeControl(defaultZoomControl);
+
 
 //1. create a text box on front end and a enter input btn, remeber to set id for the textbox and btn
 //2. pass the input value into the textbox and get the value
--- readMe.md
+++ readMe.md
@@ -1,1 +1,38 @@
-this is my read me.
+TaxiFinder
+TaxiFinder is an application that allows users to find nearby taxis on a visual map. The application uses location data from the user's device to display a map with the nearby locations of taxis.
+
+Features
+Real-time display of taxi locations on a map
+Display taxis within the radius of 0.2km to user's location
+Zoom in to desired area
+
+Getting Started
+Prerequisites
+Node.js (version 10 or higher)
+npm (version 6 or higher)
+
+Installation
+Clone the repository: git clone https://github.com/josiahyoong/josiahyoong.github.io
+
+
+Usage
+Upon opening the app, Wait for a 10 seconds refresh before the taxi locations are displayed.
+View taxi locations on the Singapore map as a default.
+To view taxi locations within a 0.2km radius circle around the user's location.
+    1. Select the "User Location' button
+    2. Key in postal code into the textbox and select the "GO" button.
+Taxi locations are refreshed every 10 seconds.
+For feedback, fill in the "Email" textbox and "Comments" textbox, select "Submit" button.
+
+Technologies Used
+HTML
+Javascript 
+
+License
+No License.
+
+Acknowledgments
+Bootstrap
+Leaflet
+OpenStreetMap for their open data
+ChatGPT Mar 23 Version
